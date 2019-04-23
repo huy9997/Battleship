e4b5c875b9728a8469d00e2184600becd0b07e2e
Huy Nguyen <huy9997@gmail.com>
2019-04-23 12:15:08 -0700

    working on the grid game board

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,13 +1,36 @@
 import React from  'react'
+import styled from 'styled-components'
 
-let width = 10; 
-let height = 10;   
+const GameScreen = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const GridContainer =styled.div` 
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  background-color: #2196F3;
+`;
+
+const SquareBlock = styled(GridContainer)`
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+`;
+
+
+let widthBlocks = ['a','b','c','d','e','f','g','e','f','g']; 
+let heightBlocks = [1,2,3,4,5,6,7,8,9,10];   
 
 let board = ()=>{
   return(
-    <div>
-      hello;
-    </div>
+    <GameScreen>
+      <GridContainer>
+        {heightBlocks.map((items)=> <SquareBlock> {items}</SquareBlock>)}
+      </GridContainer>
+      
+    </GameScreen>
   );
 }
 export default board
--- src/screens/battleship.js
+++ src/screens/battleship.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import GameBoard from '../components/gameBoard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <text>
-          Hello world
-        </text>
+      <div >
+        <GameBoard/>
       </div>
     );
   }
