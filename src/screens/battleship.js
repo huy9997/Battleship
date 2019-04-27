c7a98932a5d875d7a7c5b34bf2ef382324a6b4a4
Huy Nguyen <huy9997@gmail.com>
2019-04-27 12:15:37 -0700

    working on the battle ship board

--- src/screens/battleship.js
+++ src/screens/battleship.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const ScreenContainer = styled.div`
-  width:100vw;
-  height:100vh; 
+  width: 100vw;
+  height: 100vh;
 `;
 const GeneralScreen = styled(ScreenContainer)`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
 `;
 const GameScreen = styled(GeneralScreen)`
-  flex:8;
-  background-color:purple;
+  flex: 8;
+  background-color: purple;
 `;
 const GameControls = styled(GeneralScreen)`
-  flex:2;
-  background-color:blue;
+  flex: 2;
+  background-color: blue;
 `;
 
-
-const GridContainer =styled.div` 
+const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(10, 50px);
 `;
@@ -29,56 +28,48 @@ const CircularButton = styled.button`
   width: 50px;
   background-color: #bbb;
   border-radius: 50%;
-`
+`;
 
-let widthBlocks = [1,2,3,4,5,6,7,8,9,10]; 
-let heightBlocks = [1,2,3,4,5,6,7,8,9,10];   
+let widthBlocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let heightBlocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      player1_Ships:3,
-      player1_ShipPositions:[[]],
-      player1_Attack:[[]],
+      player1_Ships: 3,
+      player1_ShipPositions: [[]],
+      player1_Attack: [[]],
 
-      player2_Ships:3,
-      player2_ShipPositions:[[]],
-      player2_Attack:[],
+      player2_Ships: 3,
+      player2_ShipPositions: [[]],
+      player2_Attack: [],
 
-      gameStart:false,
-      player_SetupShips:'',
+      gameStart: false,
+      player_SetupShips: "",
 
-    }
+      map: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10][(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+    };
   }
-  buttonPress = e=>{
-    this.setState({
-      
-      
-    })
-  }
-  
+  buttonPress = e => {
+    console.log("onclick");
+  };
+
   render() {
-    
-
     return (
       <ScreenContainer>
         <GeneralScreen>
-        <GameScreen>
-          <GridContainer>
-          {
-            heightBlocks.map(()=> 
-              widthBlocks.map(()=> 
-              <CircularButton onClick={this.buttonPress}>
-
-              </CircularButton>))
-          }
-        </GridContainer>
-        </GameScreen>
-        <GameControls>
-        </GameControls>
-      </GeneralScreen>
+          <GameScreen>
+            <GridContainer>
+              {heightBlocks.map(col =>
+                widthBlocks.map(row => (
+                  <CircularButton onClick={this.buttonPress} />
+                ))
+              )}
+            </GridContainer>
+          </GameScreen>
+          <GameControls />
+        </GeneralScreen>
       </ScreenContainer>
-      
     );
   }
 }
